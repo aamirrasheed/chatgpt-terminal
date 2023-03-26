f5fba54290b93abb60aabcb255502fd0919a7b41
Aamir Rasheed <aamir1rasheed@gmail.com>
2023-03-26 15:22:11 -0700

    finished README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # chatgpt-terminal
+
+## Installation
+
+`curl -sSL https://raw.githubusercontent.com/aamirrasheed/chatgpt-terminal/main/install.sh | zsh`
+
+## Usage
+
+Open your terminal and type `chatgpt` to begin your session.
--- install.sh
+++ install.sh
@@ -14,8 +14,22 @@ curl -sSL "$script_url" -o "$install_dir/$script_name"
 # Set the right permissions for the script
 chmod +x "$install_dir/$script_name"
 
+case "$SHELL" in
+  */zsh)
+    config_file="$HOME/.zshrc"
+    ;;
+  */bash)
+    config_file="$HOME/.bashrc"
+    ;;
+  *)
+    echo "Unsupported shell. Finish installation by adding the following alias to your shell config file manually:"
+    echo "alias $script_name=\"$install_dir/$script_name\""
+    exit 1
+    ;;
+esac
+
 # Add an alias to the user's shell configuration file
-echo "alias $script_name=\"$install_dir/$script_name\"" >> "$HOME/.zshrc"
+echo "alias $script_name=\"$install_dir/$script_name\"" >> "$config_file"
 
 # Source the shell configuration file
 source "$HOME/.zshrc"
